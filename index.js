e8ec12ed486e0848f69eaf2f9fa4398e820a7281
Roxana <roxanafetea@yahoo.com>
2018-04-24 04:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber!==undefined){
-    
+    cart.splice();
     }
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }else{
