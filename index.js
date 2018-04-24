3c7e72a4a5dbbcb5eb242743d3f74af97c03c09a
Roxana <roxanafetea@yahoo.com>
2018-04-24 04:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,6 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber!==undefined){
     cart.splice(0,cart.length-1);
-    }
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }else{
     return 'Sorry, we don\'t have a credit card on file for you.';
